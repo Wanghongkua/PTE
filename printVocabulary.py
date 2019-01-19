d81b41d04fd7b3b0b0e8d9738a821bbf53943756
Wanghongkua <728261774@qq.com>
2019-01-19 16:04:49 +1100

    cleaning

--- printVocabulary.py
+++ printVocabulary.py
@@ -1,4 +1,3 @@
-import string
 import sys
 import re
 from googletrans import Translator
@@ -13,28 +12,6 @@ try:
 except Exception as e:
     print("Please input the file format you want.")
     raise e
-
-
-def getApproximateArialStringWidth(st):
-    size = 0  # in milinches
-    for s in st:
-        if s in 'lij|\' ':
-            size += 37
-        elif s in '![]fI.,:;/\\t':
-            size += 50
-        elif s in '`-(){}r"':
-            size += 60
-        elif s in '*^zcsJkvxy':
-            size += 85
-        elif s in 'aebdhnopqug#$L+<>=?_~FZT' + string.digits:
-            size += 95
-        elif s in 'BSPEAKVXY&UwNRCHD':
-            size += 112
-        elif s in 'QGOMm%W@':
-            size += 135
-        else:
-            size += 50
-    return size * 6 / 1000.0  # Convert to picas
 
 
 def initFPDF():
